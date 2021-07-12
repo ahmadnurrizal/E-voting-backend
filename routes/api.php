8918fa2873f087bc93b4290ba6ce4a216fd4a2a7
ahmadnur.rizal <54085895+ahmadnurrizal@users.noreply.github.com>
2021-07-13 02:21:12 +0800

    senctum setup, protecting routes
    Add senctum
    Add personal access token table
    Controller : AuthController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Models\Poll;
 use App\Http\Controllers\PollController;
 use Illuminate\Http\Request;
@@ -16,8 +17,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('polls', PollController::class);
-Route::get('polls/search/{title}', [PollController::class, 'search']);
+// Route::resource('polls', PollController::class);
+
+// Public Routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::get('/polls', [PollController::class, 'index']);
+Route::get('/polls/{id}', [PollController::class, 'show']);
+Route::get('/polls/search/{title}', [PollController::class, 'search']);
 
 // Route::get('/polls', function () {
 //   return Poll::create([
@@ -26,8 +32,15 @@ Route::get('polls/search/{title}', [PollController::class, 'search']);
 //     'description' => 'product 1 balblabla',
 //     'deadline' => 'deadline 1',
 //     'status' => 'terbuka'
-//   ]);
+//   ]); 
 // });
+
+// Protected Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+  Route::post('/polls', [PollController::class, 'create']);
+  Route::put('/polls/{id}', [PollController::class, 'update']);
+  Route::delete('/polls/{id}', [PollController::class, 'destroy']);
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
   return $request->user();
