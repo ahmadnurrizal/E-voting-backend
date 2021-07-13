fa9a0fb043b2d3b658e1f76a3f7e62ee319d9b12
ahmadnur.rizal <54085895+ahmadnurrizal@users.noreply.github.com>
2021-07-13 14:25:42 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -26,19 +26,9 @@ Route::get('/polls', [PollController::class, 'index']);
 Route::get('/polls/{id}', [PollController::class, 'show']);
 Route::get('/polls/search/{title}', [PollController::class, 'search']);
 
-// Route::get('/polls', function () {
-//   return Poll::create([
-//     'user_id' => '99',
-//     'title' => 'poll 1',
-//     'description' => 'product 1 balblabla',
-//     'deadline' => 'deadline 1',
-//     'status' => 'terbuka'
-//   ]); 
-// });
-
 // Protected Routes (need a valid token to access)
 Route::group(['middleware' => ['auth:sanctum']], function () {
-  Route::post('/polls', [PollController::class, 'create']);
+  Route::post('/polls', [PollController::class, 'store']);
   Route::put('/polls/{id}', [PollController::class, 'update']);
   Route::delete('/polls/{id}', [PollController::class, 'destroy']);
   Route::post('/logout', [AuthController::class, 'logout']);
