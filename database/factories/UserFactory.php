db128f3ee5964b55f7506f3c444311ac2ec70d87
ahmadnur.rizal <54085895+ahmadnurrizal@users.noreply.github.com>
2021-07-29 11:43:57 +0800

    set password for dummy user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,7 @@ class UserFactory extends Factory
       'name' => $faker->name,
       'email' => $faker->unique()->safeEmail,
       'email_verified_at' => now(),
-      'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+      'password' => '123456789', // password
       'remember_token' => Str::random(10),
       'date_of_birth' => $this->faker->date($format = 'Y-m-d', $max = 'now'),
       'address' => $faker->address,
