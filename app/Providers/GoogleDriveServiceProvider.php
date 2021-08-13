1a7040010664d7557572008df7c2e7b1e8123961
ahmadnur.rizal <54085895+ahmadnurrizal@users.noreply.github.com>
2021-08-13 13:36:07 +0800

    Update GoogleDriveServiceProvider.php

--- app/Providers/GoogleDriveServiceProvider.php
+++ app/Providers/GoogleDriveServiceProvider.php
@@ -18,9 +18,12 @@ class GoogleDriveServiceProvider extends ServiceProvider
   {
     Storage::extend('google', function ($app, $config) {
       $client = new \Google_Client();
-      $client->setClientId($config['clientId']);
-      $client->setClientSecret($config['clientSecret']);
-      $client->refreshToken($config['refreshToken']);
+      $client->setClientId(env('GOOGLE_DRIVE_CLIENT_ID'));
+      $client->setClientSecret(env('GOOGLE_DRIVE_CLIENT_SECRET'));
+      $client->refreshToken(env('GOOGLE_DRIVE_REFRESH_TOKEN'));
+      $client->setRedirectUri($this->_redirectURI);
+      $client->setAccessType('offline');
+      $client->setApprovalPrompt('force');
       $service = new \Google_Service_Drive($client);
       $adapter = new GoogleDriveAdapter($service, $config['folderId']);
 
