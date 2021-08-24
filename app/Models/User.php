fe23c1417399d91fc38c70ca5877b399bf5a0781
ahmadnur.rizal <54085895+ahmadnurrizal@users.noreply.github.com>
2021-08-24 15:34:10 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,59 +12,59 @@ use App\Notifications\ResetPasswordNotification;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-  use HasFactory, Notifiable, HasApiTokens;
+    use HasFactory, Notifiable, HasApiTokens;
 
-  /**
-   * The attributes that are mass assignable.
-   *
-   * @var array
-   */
-  protected $fillable = [
-    'name',
-    'email',
-    'password',
-    'date_of_birth',
-    'address',
-    'gender',
-    'profil_path',
-    'status',
-    'username'
-  ];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'date_of_birth',
+        'address',
+        'gender',
+        'profil_path',
+        'status',
+        'username'
+    ];
 
-  /**
-   * The attributes that should be hidden for arrays.
-   *
-   * @var array
-   */
-  protected $hidden = [
-    'password',
-    'remember_token',
-  ];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
-  /**
-   * The attributes that should be cast to native types.
-   *
-   * @var array
-   */
-  protected $casts = [
-    'email_verified_at' => 'datetime'
-  ];
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime'
+    ];
 
-  public function polls()
-  {
-    return $this->hasMany(Poll::class); // User can create many polls
-  }
+    public function polls()
+    {
+        return $this->hasMany(Poll::class); // User can create many polls
+    }
 
-  public function voters()
-  {
-    return $this->hasMany(Voter::class); // user can vote on many polls
-  }
+    public function voters()
+    {
+        return $this->hasMany(Voter::class); // user can vote on many polls
+    }
 
-  public function sendPasswordResetNotification($token)
-  {
+    public function sendPasswordResetNotification($token)
+    {
 
-    $url = 'https://finger-vote-2021.herokuapp.com?token=' . $token;
+        $url = 'http://finger-vote-2021.herokuapp.com/api/v1/reset-password?token=' . $token;
 
-    $this->notify(new ResetPasswordNotification($url));
-  }
+        $this->notify(new ResetPasswordNotification($url));
+    }
 }
